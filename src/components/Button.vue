5e65b5a96f09f60b0a327d28e7b02ea2f739ffb4
eevee <emen22@student.bth.se>
2024-09-20 23:05:35 +0200

    Just a text file with the commit message.

---
+++ src/components/Button.vue
@@ -0,0 +1,44 @@
+<template>
+    <!-- Use props.text to display dynamic button text -->
+    <button @click="handleClick" class="btn">{{ text }}</button>
+  </template>
+  
+
+  <script>
+  export default {
+    name: 'AButton',
+    props: {
+      text: {
+        type: String,
+        default: 'Click me'
+      },
+      customStyle: {
+        type: Object,
+        default: () => ({})
+      },
+      customClass: {
+        type: String,
+        default: ''
+      }
+    },
+    methods: {
+      handleClick() {
+        // Emit a 'click' event with custom payload if needed
+        this.$emit('click');
+      }
+    }
+  }
+  </script>
+
+<style scoped>
+.btn {
+    padding: 10px 20px;
+    background-color: #4CAF50;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    font-size: 16px;
+} 
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,27 @@
-<script setup lang="ts">
+<script lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+import AButton from '../components/Button.vue'
+
+export default {
+  components: {
+    TheWelcome,
+    AButton,
+    AButton
+  },
+  methods: {
+    handleLoginClick() {
+      console.log('Login button clicked')
+      window.location.href = 'http://192.168.1.234:9999/oauth/google/login'
+      // window.location.href = 'http://localhost:9999/oauth/google/login';
+    }
+  }
+}
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <AButton text="Google test" customClass="my-custom-class" @click="handleLoginClick" />
+    <AButton text="GitHub test" customClass="my-custom-class" @click="handleLoginClick" />
   </main>
 </template>
