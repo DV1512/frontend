07050a984617f756d5c36df2c7066426864614e2
eevee <emen22@student.bth.se>
2024-09-26 17:12:11 +0200

    updated ESLink/Prettier 2.0

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,12 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier/skip-formatting',
-    'prettier', // Lägg till Prettier-plugin
+    'prettier' // Lägg till Prettier-plugin
   ],
   parserOptions: {
     ecmaVersion: 'latest'
@@ -16,5 +16,5 @@ module.exports = {
   plugins: ['prettier'],
   rules: {
     'prettier/prettier': 'error', // ESLint visar Prettier-fel
-  },
+  }
 }
