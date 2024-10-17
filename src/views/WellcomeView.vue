0ed8fc316fcb55c5d9a5aaa2ec4eeb23a7fefb6c
muaathaw <muaw22@student.bth.se>
2024-10-17 12:02:20 +0200

    NOT ready, just to test

--- src/views/WellcomeView.vue
+++ src/views/WellcomeView.vue
@@ -1,3 +1,49 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+function handleLogoutClick() {
+  console.log('Logging out...')
+  // Add your logout logic here
+}
+</script>
+<template>
+  <section class="section">
+    <div class="container">
+      <h1 class="title has-text-centered">Welcome Page</h1>
+      <div class="box">
+        <div class="columns is-centered mt-5">
+          <div class="column is-narrow">
+            <button class="button is-primary custom-button" @click="handleLogoutClick">
+              Logout
+            </button>
+          </div>
+        </div>
+      </div>
+      <header class="header">
+        <nav class="nav">
+          <ul>
+            <li><button class="button is-primary">About</button></li>
+            <li><button class="button is-small">Button</button></li>
+            <li><a href="#">Documentation</a></li>
+          </ul>
+        </nav>
+      </header>
+      <main class="main-content">
+        <h1>ThreatMapper</h1>
+        <h2>Welcome</h2>
+        <p>Short info about the web<br />Wellcome to ThreatMapper chatbot.</p>
+        <p>To continue, scroll down<br />or</p>
 
-
+        <p>
+          Press Here to start a conversation, however the service will not be limited but saved.
+        </p>
+        <button class="Chat-btn">Start a Chat</button>
+      </main>
+      <footer class="footer">
+        <p>
+          More Info<br />
+          What? How? Why?
+        </p>
+        <p>Terms of service, contact, other links?</p>
+      </footer>
+    </div>
+  </section>
+</template>
