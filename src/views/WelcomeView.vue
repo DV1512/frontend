415694ac7321ff274ecb2886718be03bea371bc2
muaathaw <muaw22@student.bth.se>
2024-10-20 18:26:54 +0200

    the view should be seenable now but it doesn´t work

--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -1,62 +1,27 @@
-<script setup lang="ts">
-function handleLogoutClick() {
-  console.log('Logging out...')
-  // Add your logout logic here
-}
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <section class="section">
-    <div class="container">
-      <h1 class="title has-text-centered">Welcome Page</h1>
-      <div class="box">
-        <div class="columns is-centered mt-5">
-          <div class="column is-narrow">
-            <button class="button is-primary" @click="handleLogoutClick">Logout</button>
-          </div>
-        </div>
+  <div class="content is-large">
+    <h1>Welcome</h1>
+    <h1 class="title">ThreatMapper</h1>
+    <p>Short info about the web<br />Welcome to ThreatMapper chatbot.</p>
+    <p>
+      To continue,<strong>Press Here to start a conversation</strong>, however the service will not
+      be limited but saved.
+    </p>
+    <button class="button is-link is-large">Start a Chat</button>
+    <footer class="footer">
+      <div class="content has-text-centered">
+        <p>More Info<br />What? How? Why?</p>
+        <p>Terms of service, contact, other links?</p>
       </div>
-      <div class="box">Welcome.</div>
-      <header class="header">
-        <nav class="navbar">
-          <div class="navbar-menu">
-            <div class="navbar-start">
-              <a class="navbar-item">About</a>
-              <a class="navbar-item">Button</a>
-              <a class="navbar-item">Documentation</a>
-            </div>
-          </div>
-        </nav>
-      </header>
-      <main class="main-content">
-        <h1 class="title">ThreatMapper</h1>
-        <h2 class="subtitle">Welcome</h2>
-        <p>Short info about the web<br />Welcome to ThreatMapper chatbot.</p>
-        <p>To continue, scroll down<br />or</p>
-        <p>
-          Press Here to start a conversation, however the service will not be limited but saved.
-        </p>
-        <button class="button is-link is-large">Start a Chat</button>
-      </main>
-      <footer class="footer">
-        <div class="content has-text-centered">
-          <p>More Info<br />What? How? Why?</p>
-          <p>Terms of service, contact, other links?</p>
-        </div>
-      </footer>
-    </div>
-  </section>
+    </footer>
+  </div>
 </template>
 
 <style scoped>
-.header {
-  background-color: #333;
-  color: white;
-  padding: 1rem;
-}
-
-.main-content {
-  padding: 2rem;
+.content {
+  text-align: center;
 }
 
 .footer {
