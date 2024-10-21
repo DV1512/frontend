eacb9119ea855ca04e733ee6449e9b9699e84f19
muaathaw <muaw22@student.bth.se>
2024-10-21 09:51:28 +0200

    router/button

--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -7,7 +7,9 @@
         <div class="content">
           <h1>Welcome to</h1>
           <h1 class="title">ThreatMapper AI</h1>
-          <button class="button is-primary custom-button">Press to start a conversation</button>
+          <RouterLink to="/chat">
+            <button class="button is-primary custom-button">Start chat</button>
+          </RouterLink>
         </div>
       </div>
     </div>
