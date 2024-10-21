c45a2bbb47c463afa96b027fe8a2362ab6fd4c23
muaathaw <muaw22@student.bth.se>
2024-10-21 09:48:08 +0200

    Fixed all the comments

--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -3,19 +3,21 @@
 <template>
   <section class="section">
     <div class="container">
-      <div class="content is-large">
-        <h1>Welcome to</h1>
-        <h1 class="title">ThreatMapper AI</h1>
-        <button class="button is-primary start-button">Press to start a conversation</button>
+      <div class="box">
+        <div class="content">
+          <h1>Welcome to</h1>
+          <h1 class="title">ThreatMapper AI</h1>
+          <button class="button is-primary custom-button">Press to start a conversation</button>
+        </div>
       </div>
     </div>
   </section>
 </template>
 
 <style scoped>
-.start-button {
-  width: 250px; /* Adjust the width as needed */
-  margin: 0 80px; /* Add horizontal margin to create space between buttons */
+.custom-button {
+  width: 250px;
+  margin: 0 80px;
 }
 .content {
   text-align: center;
