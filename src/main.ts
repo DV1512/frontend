22e9a34528d3858400431a1263986d2f5df748c8
eevee <emen22@student.bth.se>
2024-11-04 15:38:51 +0100

    Test code removed

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,6 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import { passwordLogin } from 'sdk'
-
-passwordLogin('emma.ehnpaulsson@gmail.com', 'test').then((res) => {
-  console.log(res)
-})
 
 const app = createApp(App)
 
